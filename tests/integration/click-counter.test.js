8da758aef61719ee08de612df7d2c66a9e5ae9a9
ggorlen <gsgorlen@gmail.com>
2022-02-10 19:38:05 -0800

    fix timeouts and test url

--- tests/integration/click-counter.test.js
+++ tests/integration/click-counter.test.js
@@ -1,7 +1,6 @@
 const puppeteer = require("puppeteer");
 
-jest.setTimeout = 10000;
-const url = "http://localhost:8000/dist/click-counter.html"; // TODO move to env
+const url = "http://localhost:8000/dist"; // TODO move to env
 
 describe("click counter", () => {
   let browser;
@@ -15,6 +14,7 @@ describe("click counter", () => {
   });
   beforeEach(async () => {
     page = await browser.newPage();
+    await page.setDefaultTimeout(10000);
     await page.goto(url, {timeout: 35000});
     await page.waitForFunction(() => {
       const el = document.getElementById("status");
